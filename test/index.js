895e7e06d488c53286408606951ee63f4a55ac98
Nicola Pietroluongo <nik.longstone@gmail.com>
2017-04-09 17:39:17 +0100

    add .npmignore, fix style, refactor package.json

--- test/index.js
+++ test/index.js
@@ -3,14 +3,14 @@ var AmazonDateParser = require('../index');
 
 describe('AmazonDateParser', function() {
 
-    describe('constructor', function() {
+    describe('varructor', function() {
 
-        it('should throw an error if the constructor parameter is not provided', function() {
-          assert.throws(function(){new AmazonDateParser()}, Error);
+        it('should throw an error if the varructor parameter is not provided', function() {
+          assert.throws(function(){new AmazonDateParser();}, Error);
         });
 
         it('should return a correct date range given a "right now" date', function() {
-            const rawDate = 'PRESENT_REF';
+            var rawDate = 'PRESENT_REF';
             var now = new Date(Date.now());
             var expected = new Date(now.getFullYear(), now.getMonth(), now.getDay(), now.getHours(), now.getMinutes());
 
@@ -24,7 +24,7 @@ describe('AmazonDateParser', function() {
         });
 
         it('should return a correct date range given a single day', function() {
-            const rawDate = '2017-01-30';
+            var rawDate = '2017-01-30';
             var startDate = new Date(rawDate).setUTCHours(0,0,0,0);
             var endDate = new Date(rawDate).setUTCHours(23, 59, 59, 999);
             expected = {
@@ -39,7 +39,7 @@ describe('AmazonDateParser', function() {
 
 
         it('should return a correct date range given a week', function() {
-            const rawDate = '2017-W13'; //March 27, 2017 to April 2, 2017
+            var rawDate = '2017-W13'; //March 27, 2017 to April 2, 2017
             var startDate = new Date('2017-03-27');
             var endDate = new Date('2017-04-02').setUTCHours(23, 59, 59, 999);
             expected = {
@@ -53,7 +53,7 @@ describe('AmazonDateParser', function() {
         });
 
         it('should return a correct date range given a weekend', function() {
-            const rawDate = '2017-W13-WE'; // April 1, 2017 to April 2, 2017
+            var rawDate = '2017-W13-WE'; // April 1, 2017 to April 2, 2017
             var startDate = new Date('2017-04-01');
             var endDate = new Date('2017-04-02').setUTCHours(23, 59, 59, 999);
             expected = {
@@ -67,7 +67,7 @@ describe('AmazonDateParser', function() {
         });
 
         it('should return a correct date range given a month', function() {
-            const rawDate = '2017-01';
+            var rawDate = '2017-01';
             var startDate = new Date(rawDate + '-01').setUTCHours(0,0,0,0);
             var endDate = new Date(rawDate + '-31').setUTCHours(23, 59, 59, 999);
             expected = {
@@ -81,7 +81,7 @@ describe('AmazonDateParser', function() {
         });
 
         it('should return a correct date range given a year', function() {
-            const rawDate = '2017';
+            var rawDate = '2017';
             var startDate = new Date(rawDate + '-01-01').setUTCHours(0,0,0,0);
             var endDate = new Date(rawDate + '-12-31').setUTCHours(23, 59, 59, 999);
             expected = {
@@ -95,7 +95,7 @@ describe('AmazonDateParser', function() {
         });
 
         it('should return a correct date range given a decade', function() {
-            const rawDate = '201X'; // 2010 to 2019
+            var rawDate = '201X'; // 2010 to 2019
             var startDate = new Date('2010-01-01');
             var endDate = new Date('2019-12-31').setUTCHours(23, 59, 59, 999);
             expected = {

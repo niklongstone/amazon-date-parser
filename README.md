c2b97aaaeb4d3114e28a1803c1298d1e3b37f17c
Nicola Pietroluongo <nik.longstone@gmail.com>
2017-06-09 00:22:02 +0100

    add seasons support

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Given an [AMAZON.DATE](https://developer.amazon.com/public/solutions/alexa/alexa
 ## Installation
 `npm install amazon-date-parser`
 
-## Usage
+## Basic usage
 ```
 var AmazonDateParser = require('amazon-date-parser');
 
@@ -20,8 +20,9 @@ returns:
     endDate: Tue Jan 31 2017 23:59:59 GMT+0000 (GMT)
 }
 */
+
 ```
-It throws an error in case of invalid constructor parameter or parameter not supported.
+It throws an error when the date is not valid or not supported.
 ```
 var AmazonDateParser = require('amazon-date-parser');
 
@@ -35,20 +36,79 @@ try {
 ```
 
 ## Input values:
-single day: 2015-11-24
-week: 2015-W48
-weekend: 2015-W48-WE
-month: 2015-11
-year: 2016
-decade: 201X
+* single day: `2017-11-24`
+* week: `2017-W48`
+* weekend: `2017-W48-WE`
+* month: `2017-11`
+* seasons: `2017-SP`, `2017-WI`, `2017-FA`, `2017-SU` (see below)
+* year: `2018`
+* decade: `201X`
 
-## RoadMap
-The next release will have support for:
-winter: 2017-WI
-spring: 2017-SP
-summer: 2017-SU
-autumn/fall: 2017-FA
+## Seasons
+The constructor receives the following __optional__ parameters only valid for season's calculations.
+```
+{
+    hemisphere: 'N',    //`N` for North, `S` for the South hemisphere (default is `N`)
+    seasons: { // see Custom seasons for more info about this parameter
+        // ...
+        'SU': { // summer
+            startDate: [5, 1],  // 1st of June
+            endDate: [8, 0]     // 31st of August
+        }
+        // ...
+    }
+}
+```
+The `seasons` parameters overrides all the others. For instance, if you provide `seasons` and `hemisphere` the latter will be ignored.
 
+### Default seasons
+The north hemisphere seasons are by default defined as follow:
+ * Spring, from the 1st of March to the 31st of May
+ * Summer, from the 1st of June to the 31st of August
+ * Fall/Autumn, from the 1st of September to the 30th of November
+ * Winter, from the 1st of December to the end of February (28th or 29th depending on leap year)
+
+For the south hemisphere you should construct the Amazon date parser as follow:  
+`var date = new AmazonDateParser({hemisphere: 'S'});`  
+and you can get:  
+* Spring, from the 1st of September to the 30th of November
+* Summer, from the 1st of December to the end of February (28th or 29th depending on leap year)
+* Fall/Autumn, from the 1st of March to the 31st of May
+* Winter, from the 1st of June to the 31st of August
+
+### Custom seasons
+You can pass your own seasons representation using the following configurations:
+```
+var options = {
+    seasons: {
+        'SP': {
+            startDate: [2, 1],  // 1st of March
+            endDate: [3, 0]     // end of March
+        },
+        'SU': {
+            startDate: [5, 1],  // 1st of June
+            endDate: [8, 0]     // end of August
+        },
+        'FA': {
+            startDate: [8, 1],  // 1st of September
+            endDate: [11, 0]    // end of November
+        },
+        'WI': {
+            startDate: [11, 1], // 1st of December
+            endDate: [2, 0]     // end of February (28th or 29th)
+        }
+}
+var AmazonDateParser = require('amazon-date-parser');
+
+var date = new AmazonDateParser('2018-SP', options);
+/*
+returns something like:
+{
+    startDate: Thu Mar 01 2018 00:00:00 GMT+0000 (GMT),
+    endDate: Sat Mar 31 2018 23:59:59 GMT+0000 (GMT)
+}
+*/
+```
 
 ## Credits
 
--- test/index.js
+++ test/index.js
@@ -3,9 +3,9 @@ var AmazonDateParser = require('../index');
 
 describe('AmazonDateParser', function() {
 
-    describe('varructor', function() {
+    describe('constructor', function() {
 
-        it('should throw an error if the varructor parameter is not provided', function() {
+        it('should throw an error if the constructor parameter is not provided', function() {
           assert.throws(function(){new AmazonDateParser();}, Error);
         });
 
@@ -20,21 +20,17 @@ describe('AmazonDateParser', function() {
 
             assert.deepEqual(date.startDate, date.endDate, 'startDate and endDate are not equal');
             //does not compare seconds because can fail during a slow test execution.
-            assert.equal(resDate.toString(), expected.toString());            
+            assert.equal(resDate.toString(), expected.toString());
         });
 
         it('should return a correct date range given a single day', function() {
             var rawDate = '2017-01-30';
             var startDate = new Date(rawDate).setUTCHours(0,0,0,0);
             var endDate = new Date(rawDate).setUTCHours(23, 59, 59, 999);
-            expected = {
-                startDate: new Date(startDate),
-                endDate: new Date(endDate)
-            };
 
             var date = new AmazonDateParser(rawDate);
 
-            assert.deepEqual(date, expected);
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
         });
 
 
@@ -42,14 +38,10 @@ describe('AmazonDateParser', function() {
             var rawDate = '2017-W13'; //March 27, 2017 to April 2, 2017
             var startDate = new Date('2017-03-27');
             var endDate = new Date('2017-04-02').setUTCHours(23, 59, 59, 999);
-            expected = {
-                startDate: new Date(startDate),
-                endDate: new Date(endDate)
-            };
 
             var date = new AmazonDateParser(rawDate);
 
-            assert.deepEqual(date, expected);
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
         });
 
         it('should return a correct date range given a weekend', function() {
@@ -63,21 +55,156 @@ describe('AmazonDateParser', function() {
 
             var date = new AmazonDateParser(rawDate);
 
-            assert.deepEqual(date, expected);
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
         });
 
         it('should return a correct date range given a month', function() {
             var rawDate = '2017-01';
             var startDate = new Date(rawDate + '-01').setUTCHours(0,0,0,0);
             var endDate = new Date(rawDate + '-31').setUTCHours(23, 59, 59, 999);
-            expected = {
-                startDate: new Date(startDate),
-                endDate: new Date(endDate)
-            };
 
             var date = new AmazonDateParser(rawDate);
 
-            assert.deepEqual(date, expected);
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a spring season', function() {
+            var rawDate = '2017-SP'; // 1st of March to 31st of May
+            var startDate = Date.UTC(2017, 2, 1);
+            var endDate = new Date(2017, 5, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate);
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a summer season', function() {
+            var rawDate = '2017-SU'; // 1st of June to 31st of August
+            var startDate = Date.UTC(2017, 5, 1);
+            var endDate = new Date(2017, 8, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate);
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a fall season', function() {
+            var rawDate = '2017-FA'; // 1st of September to 30th of November
+            var startDate = Date.UTC(2017, 8, 1);
+            var endDate = new Date(2017, 11, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate);
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a winter season', function() {
+            var rawDate = '2017-WI'; // 1st of December to end of February 28th
+            var startDate = Date.UTC(2017, 11, 1);
+            var endDate = new Date(2018, 2, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate);
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a winter season on a leap year', function() {
+            var rawDate = '2019-WI'; // 1st of December to end of February 29th
+            var startDate = Date.UTC(2019, 11, 1);
+            var endDate = new Date(2020, 2, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate);
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a winter on south hemisphere', function() {
+            var rawDate = '2017-WI'; // 1st of June to 31st of August
+            var startDate = Date.UTC(2017, 5, 1);
+            var endDate = new Date(2017, 8, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate, {hemisphere: 'S'});
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a spring on south hemisphere', function() {
+            var rawDate = '2017-SP'; // 1st of September to 30th of November
+            var startDate = Date.UTC(2017, 8, 1);
+            var endDate = new Date(2017, 11, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate, {hemisphere: 'S'});
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a summer on south hemisphere', function() {
+            var rawDate = '2017-SU'; // 1st of December to end of February 28th
+            var startDate = Date.UTC(2017, 11, 1);
+            var endDate = new Date(2018, 2, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate, {hemisphere: 'S'});
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a fall on south hemisphere', function() {
+            var rawDate = '2017-FA'; // 1st of March to 31st of May
+            var startDate = Date.UTC(2017, 2, 1);
+            var endDate = new Date(2017, 5, 0, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate, {hemisphere: 'S'});
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range given a custom seasons options', function() {
+            var rawDate = '2017-SU';
+            var options = {
+                'seasons':{
+                    'SU': {
+                        startDate: [0, 1],
+                        endDate: [1, 2]
+                    }
+                }
+            };
+            var startDate = Date.UTC(2017, 0, 1);
+            var endDate = new Date(2017, 1, 2, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate, options);
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
+        });
+
+        it('should return a correct date range ignoring the hemisphere option when the seasons one is present', function() {
+            var rawDate = '2018-SU';
+            var options = {
+                'seasons':{
+                    'SP': {
+                        startDate: [1, 0],  // 1st of March
+                        endDate: [1, 1]     // 31st of May
+                    },
+                    'SU': {
+                        startDate: [5, 0],  // 1st of June
+                        endDate: [5, 1]     // 31st of August
+                    },
+                    'FA': {
+                        startDate: [8, 0],  // 1st of September
+                        endDate: [8, 1]    // 30th of November
+                    },
+                    'WI': {
+                        startDate: [10, 0], // 1st of December
+                        endDate: [10, 1]     // end of February (28th or 29th)
+                    },
+                'hemisphere': 'S'
+                }
+            };
+            var startDate = Date.UTC(2018, 5, 0);
+            var endDate = new Date(2018, 5, 1, 23, 59, 59, 999);
+
+            var date = new AmazonDateParser(rawDate, options);
+
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
         });
 
         it('should return a correct date range given a year', function() {
@@ -98,16 +225,19 @@ describe('AmazonDateParser', function() {
             var rawDate = '201X'; // 2010 to 2019
             var startDate = new Date('2010-01-01');
             var endDate = new Date('2019-12-31').setUTCHours(23, 59, 59, 999);
-            expected = {
-                startDate: new Date(startDate),
-                endDate: new Date(endDate)
-            };
 
             var date = new AmazonDateParser(rawDate);
 
-            assert.deepEqual(date, expected);
+            assert.deepEqual(date, expectedJSON(startDate, endDate));
         });
 
     });
+
+    function expectedJSON(startDate, endDate) {
+        return {
+            startDate: new Date(startDate),
+            endDate: new Date(endDate)
+        };
+    }
 
 });
